0ee9863a665a4f6a45c021407effcbf8df67f6b8
Samuel Gunter <samathingamajig@gmail.com>
2021-03-20 10:42:14 -0500

    Added quizzing

--- interactive.py
+++ interactive.py
@@ -1,11 +1,15 @@
 from main import *
+import random
 import os
+import re
+
+equations = set()
 
 def clear_screen() -> None:
     os.system("clear")
 
-def press_continue() -> None:
-    input("Press enter to continue")
+def press_enter_to_menu() -> None:
+    input("\nPress enter to return to the menu")
 
 def solve_equations() -> None:
     print("Time to solve some equations")
@@ -21,7 +25,7 @@ def solve_equations() -> None:
         print("The coefficients are:", coeffs)
         spaceless = "".join(char for char in inp if char != " ")
         print(pretty_balanced_chem_eq(spaceless, coeffs))
-    press_continue()
+    press_enter_to_menu()
 
 def solve_equations_with_explanations() -> None:
     print("Time to solve some equations, but fancier")
@@ -41,11 +45,43 @@ def solve_equations_with_explanations() -> None:
         print("The coefficients are:", coeffs)
         spaceless = "".join(char for char in inp if char != " ")
         print(pretty_balanced_chem_eq(spaceless, coeffs))
-    press_continue()
+    press_enter_to_menu()
 
 def quiz() -> None:
+    global equations
     print("Quiz time!")
-    input()
+    print("Answer each equation with a list of coefficients, separated by a space")
+    print("Example: \"1 6 3 2\"")
+    print()
+    correct = incorrect = total = 0
+    SENTINEL = "stop"
+    while True:
+        if equations == set():
+            load_equations()
+        equation = random.choice([*equations])
+        print(f"Balance this equation: (type \"{SENTINEL}\" to stop)")
+        print(equation)
+        compound_count = len(re.split(r"\+|\=\>", equation))
+        list_validator = r"^(?:\d+\s+){" + str(compound_count - 1) + r"}\d+$"
+        while re.match(list_validator, (inp := input())) is None:
+            if inp.lower() == SENTINEL: break
+            print(f"Invalid input! Your list should only contain numbers, be separated by a single space, and be of length {compound_count}")
+        if inp.lower() == SENTINEL: break
+        official_answer, _ = balance(equation)
+        given_answer = [int(n) for n in inp.split(" ")]
+        if official_answer == given_answer:
+            print("Correct!")
+            correct += 1
+        else:
+            print(f"Incorrect! The correct coefficients are {', '.join(str(n) for n in official_answer)}")
+            incorrect += 1
+        total += 1
+        print(pretty_balanced_chem_eq(equation, official_answer))
+        print(f"Current score => Correct: {correct} ; Incorrect: {incorrect} ; Total: {total} ; Correct/Incorrect ratio: {correct / incorrect if incorrect != 0 else 'N/A'} ; Grade: {correct / total * 100}")
+        print()
+    print()
+    print(f"Final score => Correct: {correct} ; Incorrect: {incorrect} ; Total: {total} ; Correct/Incorrect ratio: {correct / incorrect if incorrect != 0 else 'N/A'} ; Grade: {correct / total * 100}")
+    press_enter_to_menu()
 
 def validate_solutions() -> None:
     print("Validated")
@@ -60,7 +96,7 @@ def about() -> None:
     }
     for key, value in parts.items():
         print(f"{key}: {value}\n")
-    press_continue()
+    press_enter_to_menu()
 
 def menu() -> None:
     clear_screen()
@@ -111,6 +147,11 @@ def menu() -> None:
         print("Goodbye!")
         exit(0)
 
+def load_equations() -> None:
+    global equations
+    with open("equations.txt", "r") as file:
+        equations = set(file.read().splitlines())
+        equations = [eq for eq in equations if len(eq) > 0 and eq[0] != "#"]
+
 if __name__ == "__main__":
-    print_logo()
     menu()
