a2e26ccea6328651b44edb52c1bd8f3f73f5eed0
Samuel Gunter <samathingamajig@gmail.com>
2021-03-20 08:34:00 -0500

    tests for the equations file

--- main_test.py
+++ main_test.py
@@ -7,8 +7,19 @@ class TestFull(unittest.TestCase):
             ["AgNO3+Na2CO3=>Ag2CO3+NaNO3", [2, 1, 1, 2]],
             ["BaCl2+K3PO4=>Ba3P2O8+KCl", [3, 2, 1, 6]]
         ]
-        for inp, out in tests:
+        for i, (inp, out) in enumerate(tests):
+            if balance(inp) != (out, ''):
+                print(i, inp, "|", out)
             self.assertEqual(balance(inp), (out, ''))
+    
+    def test_equations_txt(self):
+        with open("equations.txt", "r") as file:
+            equations = file.read().splitlines()
+            equations = [eq for eq in equations if eq != "" and eq[0] != "#"]
+        for i, eq in enumerate(equations):
+            if balance(eq)[1] != "":
+                print(i, eq)
+            self.assertEqual(balance(eq)[1], "")
 
 if __name__ == "__main__":
     unittest.main()
